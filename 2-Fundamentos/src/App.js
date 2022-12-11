d9ff0b8fcc3f5e5dbf7b31f052c7215b7377d391
Renato Shigueo Fujimoto <98925481+Renatofujimoto@users.noreply.github.com>
2022-12-11 12:03:21 -0300

    Update App.js

--- 2-Fundamentos/src/App.js
+++ 2-Fundamentos/src/App.js
@@ -1,18 +1,22 @@
-import './App.css'
-import MyForm from './components/MyForm'
+import logo from "./logo.svg";
+import "./App.css";
+
+import FirstComponent from "./components/FirstComponent";
+import TemplateExpressions from "./components/TemplateExpressions";
+import MyComponent from "./components/MyComponent";
+import Events from "./components/Events";
+import Challenge from "./components/Challenge";
 
 function App() {
   return (
     <div className="App">
-      <h1>Formularios React</h1>
-      <MyForm
-        user={{
-          name: 'Renato Shigueo Fujimoto',
-          email: 'renatofujimoto2@gmail.com'
-        }}
-      />
+      <FirstComponent />
+      <TemplateExpressions />
+      <MyComponent />
+      <Events />
+      <Challenge />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
