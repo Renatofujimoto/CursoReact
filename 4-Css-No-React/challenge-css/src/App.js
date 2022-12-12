87edad151b9a6bf978dc3bed997ff7f1f85b8ba8
Renato Shigueo Fujimoto <98925481+Renatofujimoto@users.noreply.github.com>
2022-12-12 15:07:37 -0300

    Update App.js

--- 4-Css-No-React/challenge-css/src/App.js
+++ 4-Css-No-React/challenge-css/src/App.js
@@ -1,78 +1,23 @@
-import { Fragment, useState } from 'react'
-import './App.css'
-
-import city from './assets/city.jpg'
-import CarDetails from './components/CarDetails'
-import ChangeMessageState from './components/ChangeMessageState'
-import ConditionalRender from './components/ConditionalRender'
-import Container from './components/Container'
-import ExecuteFunction from './components/ExecuteFunction'
-import ListRender from './components/ListRender'
-import ManageData from './components/ManageData'
-import MessageState from './components/MessageState'
-import ShowUserName from './components/ShowUserName'
+import logo from "./logo.svg";
+import "./App.css";
+import Car from "./components/Car";
 
 function App() {
-  const cars = [
-    { id: 1, brand: 'Ferrari', color: 'Amarelo', km: 0 },
-    { id: 2, brand: 'KIA', color: 'Branco', km: 200000 },
-    { id: 3, brand: 'Renault', color: 'Azul', km: 32000 }
-  ]
-
-  function showMessage() {
-    console.log('Evento do componente pai')
-  }
-
-  const [message, setMessage] = useState()
-
-  const handleMessage = msg => {
-    setMessage(msg)
-  }
-
+  const myCars = [
+    { name: "Fusca", km: 10000, color: "Branca" },
+    { name: "Polo", km: 32000, color: "Cinza" },
+    { name: "Onix", km: 0, color: "Preto" },
+  ];
   return (
     <div className="App">
-      <h1>Seção 3</h1>
-      <div>
-        <img src="/img1.jpg" alt="Paisagem" />
-        <img src={city} alt="Cidade" />
+      <h1>Showroom de carros</h1>
+      <div className="car-container">
+        {myCars.map((car) => (
+          <Car car={car} />
+        ))}
       </div>
-      <ManageData />
-      <ListRender />
-      <ConditionalRender />
-      {/* props */}
-      <ShowUserName name="Renato" />
-      {/* destructuring */}
-      <CarDetails brand="Ford" color="Azul" km={10000} />
-      {/* reaproveitamento */}
-      <CarDetails brand="VW" color="Vermelho" km={535} />
-      <CarDetails brand="Fiat" color="Branco" km={0} />
-      {/* loop com array de obj */}
-      {cars.map(car => (
-        <CarDetails
-          key={car.id}
-          brand={car.brand}
-          color={car.color}
-          km={car.km}
-        />
-      ))}
-      {/* fragments */}
-      <Fragment />
-      {/* children prop */}
-      <Container>
-        <p>Eu sou do componente superior</p>
-      </Container>
-      <Container>
-        <div>
-          <p>Eu também</p>
-        </div>
-      </Container>
-      {/* event as prop */}
-      <ExecuteFunction myFunction={showMessage} />
-      {/* state lift */}
-      <MessageState msg={message} />
-      <ChangeMessageState handleMessage={handleMessage} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
