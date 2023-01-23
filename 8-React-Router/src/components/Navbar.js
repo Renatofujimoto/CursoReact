3c5d1074e78ea33003832559478a96415daf8db7
Renato Shigueo Fujimoto <98925481+Renatofujimoto@users.noreply.github.com>
2023-01-23 20:07:59 -0300

    Add files via upload

---
+++ 8-React-Router/src/components/Navbar.js
@@ -0,0 +1,19 @@
+import "./Navbar.css"
+import { Link,NavLink } from "react-router-dom"
+
+const Navbar = () => {
+  return (
+  <nav>
+    {/*<Link to="/">Home</Link>
+    <Link to="/about">Sobre</Link>*/}
+
+    <NavLink to="/" // segundo jeito </nav>className={({isActive}) => (isActive ? "esta-ativo" : "nao-ativo")}
+    >Home
+    </NavLink>
+
+    <NavLink to="/about">Sobre</NavLink>    
+  </nav>
+  )
+}
+
+export default Navbar
