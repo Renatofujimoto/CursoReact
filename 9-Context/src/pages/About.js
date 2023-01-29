c57815bff0eb0d1038d787827c354b34223b7f56
Renato Shigueo Fujimoto <98925481+Renatofujimoto@users.noreply.github.com>
2023-01-28 21:53:53 -0300

    Update About.js

--- 9-Context/src/pages/About.js
+++ 9-Context/src/pages/About.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import { useContext } from 'react'
+
+import { CounterContext } from "../context/CounterContext";
+
+import { useTitleColorContext } from "../hooks/useTitleColorContext"
 
 const About = () => {
+  const { counter } = useContext(CounterContext)
+  
+  // 5 - contexto mais complexo
+  
+  const { color } = useTitleColorContext()
+  
   return (
-    <div>About</div>
+    <div>
+    
+      <h1 style= {{ color:color }}> About</h1>
+      <p>Valor contador: {counter}</p>
+    </div>
   )
 }
 
